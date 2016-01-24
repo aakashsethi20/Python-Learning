9f8e79a681574a1f2785dd0d7073caf57bf201c2
Ash Sethi <aakashsethi20@gmail.com>
2016-01-24 17:43:32 -0500

    breakTime.py complete

--- breakTime.py
+++ breakTime.py
@@ -1,5 +1,15 @@
+#!/usr/bin/python
+
 import webbrowser
 import time
 
-time.sleep(10)
-webbrowser.open("https://www.youtube.com/watch?v=TRjH_gJbUqQ")
+timeToWait = 7200
+numberOfBreaks = 5
+breakCounter = 0
+
+print "The program started at " + time.ctime()
+
+while (breakCounter < numberOfBreaks) :
+	time.sleep(timeToWait)		# program waits for 'timeToWait' seconds
+	webbrowser.open("https://www.youtube.com/watch?v=TRjH_gJbUqQ")		# link to Immigrant SOng - Led Zeppelin
+	breakCounter+=1
