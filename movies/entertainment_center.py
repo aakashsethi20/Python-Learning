b91b0f58f7500fac75e512f3862129e2348bd63d
Ash Sethi <aakashsethi20@gmail.com>
2016-01-31 02:00:36 -0500

    300 added

--- movies/entertainment_center.py
+++ movies/entertainment_center.py
@@ -1,6 +1,20 @@
 import media
+import webbrowser
+import sys
 
-toy_story = media.Movie("Toy Story", "Story of a boy and his toys who come to life.",
-	"http://cdn.collider.com/wp-content/uploads/toy-story-poster1.jpg", 
-	"https://www.youtube.com/watch?v=KYz2wyBy3kc")
-print (toy_story.story)
+toy_story = media.Movie(
+	# Title
+	"300",
+	# Story
+	"King Leonidas goes to war with 300"+
+	" Spartans against 300,000, "+
+	"soldiers of Persian Lord Xerxes.",
+	# Poster
+	"https://upload.wikimedia.org/wikipedia/en/thumb"+
+	"/5/5c/300poster.jpg/220px-300poster.jpg",
+	# Trailer
+	"https://www.youtube.com/watch?v=UrIbxk7idYA")
+# Trailer launcher
+webbrowser.open("https://www.youtube.com/watch?v=UrIbxk7idYA")
+
+#sys.exit("Trailer launched!")
