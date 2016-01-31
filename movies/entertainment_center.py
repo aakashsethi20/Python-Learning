6c9d550e12b514d214032092196ae02edd00cd91
Ash Sethi <aakashsethi20@gmail.com>
2016-01-31 01:35:38 -0500

    movie webpage

---
+++ movies/entertainment_center.py
@@ -0,0 +1,6 @@
+import media
+
+toy_story = media.Movie("Toy Story", "Story of a boy and his toys who come to life.",
+	"http://cdn.collider.com/wp-content/uploads/toy-story-poster1.jpg", 
+	"https://www.youtube.com/watch?v=KYz2wyBy3kc")
+print (toy_story.story)
---
+++ movies/media.py
@@ -0,0 +1,10 @@
+#!usr/bin/python
+
+class Movie():
+	def __init__(self, movie_title, movie_story, movie_poster, trailer_youtube_url) :
+		self.title = movie_title
+		self.story = movie_story
+		self.poster = movie_poster
+		self.trailer = trailer_youtube_url
+
+	 
